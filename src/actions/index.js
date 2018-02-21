8459956748af48ad01907d0eb0f9a81e50237a3c
Tin Chu <mchu.tinho@gmail.com>
2018-02-21 10:14:35 -0600

    axios added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
+import axios from 'axios';
 
+const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({email, password}) {
 
   return  function(dispatch) {
-    
+    axios.post(`${ROOT_URL}/signin`, {email, password});
     // Submit email/password to server
 
     // if request is good...
