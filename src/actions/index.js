ef97d151d57dcb0fb87d7f8f56f3a9e2b179a733
Tin Chu <mchu.tinho@gmail.com>
2018-02-22 10:58:23 -0600

    added redux form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,34 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({email, password}) {
 
   return  function(dispatch) {
-    axios.post(`${ROOT_URL}/signin`, {email, password});
     // Submit email/password to server
-
-    // if request is good...
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - redirect to the route '/feature'
-
-    // if request is bad...
-    // - Show an error to the user
+    axios.post(`${ROOT_URL}/signin`, {email, password})
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
+        localStorage.setItem('token', response.data.token);
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // if request is bad...
+        // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
+      });
   }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
