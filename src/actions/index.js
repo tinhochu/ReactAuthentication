7c29329082429f905ccde98ba5035ff54f50947f
Tin Chu <mchu.tinho@gmail.com>
2018-03-02 16:27:49 -0600

    finishing auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import {
   AUTH_USER,
   UNAUTH_USER,
   AUTH_ERROR,
+  FETCH_MESSAGE
 } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
@@ -30,6 +31,21 @@ export function signinUser({email, password}) {
   }
 }
 
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, {email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER});
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(response => {
+        console.log(response);
+        dispatch(authError(response));
+      })
+  }
+}
+
 export function authError(error) {
   return {
     type: AUTH_ERROR,
@@ -39,6 +55,17 @@ export function authError(error) {
 
 export function signoutUser() {
   localStorage.removeItem('token');
-  
+
   return { type: UNAUTH_USER };
+}
+
+export function fetchMessage() {
+  return function(dispatch) {
+    axios.get(ROOT_URL, {
+      headers: { authorization: localStorage.getItem('token') }
+    })
+    .then(response => {
+      dispatch({ type: FETCH_MESSAGE, payload: response.data.message })
+    })
+  };
 }
